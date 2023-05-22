172a0872fc1af8355ea31af80195c09dec344c5d
Peter Tomko <tomko5peter@gmail.com>
2023-05-22 14:04:01 +0200

    saving actual and historical cvModel

--- bin/modelling_pipeline/pl__elastic_net_estimation.py
+++ bin/modelling_pipeline/pl__elastic_net_estimation.py
@@ -347,12 +347,34 @@ print('Coefficients Written!')
 # MAGIC #### Write cvModel
 
 # COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ##### Historization
+
+# COMMAND ----------
+
 blob_path = '5_model_storage/pl/meiro_estimation/cv_model/elastic_net__' + current_dt
 WPATH = '{}/{}'.format(CONT_PATH.format('replenishment-modelling', STORAGE_ACCOUNT), blob_path)
 rem_blob_files(blob_loc=blob_path, cont_nm='replenishment-modelling', block_blob_service=BLOCK_BLOB_SERVICE)
 
 cvModel.save(WPATH)
-print('cvModel Written!')
+print('cvModel Written - Historization!')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ##### Actual
+
+# COMMAND ----------
+
+blob_path = '5_model_storage/pl/meiro_estimation/cv_model/elastic_net__actual'
+WPATH = '{}/{}'.format(CONT_PATH.format('replenishment-modelling', STORAGE_ACCOUNT), blob_path)
+rem_blob_files(blob_loc=blob_path, cont_nm='replenishment-modelling', block_blob_service=BLOCK_BLOB_SERVICE)
+
+cvModel.save(WPATH)
+print('cvModel Written - ACTUAL!')
 
 # COMMAND ----------
 
